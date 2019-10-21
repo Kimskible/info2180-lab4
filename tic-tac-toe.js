1997a12f03b6addc11e6af5cfee1c0d0f8f2771b
Kimski <k_oksana15@hotmail.com>
2019-10-20 20:37:00 -0500

    Fixed grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,19 @@
+//0 is X and 1 is O
+state = [];
+
 //Load page before js
 window.onload = () => {
   boardtable();
+
 }
 
 //Setting Grids on page + displaying squares
-
 const boardtable = () => {
-
-  let board = document.getElementById("board");
-  // let gridSquare = document.getElementsByClassName("square");
-  let blocks = board.getElementsByTagName("div");
+  var board = document.getElementById("board");
+  var blocks = board.getElementsByTagName("div");
 
   for (let i = 0; i < blocks.length; i++) {
     blocks[i].className = "square";
-  
-  }
-  
-  
+  }  
 }
+
