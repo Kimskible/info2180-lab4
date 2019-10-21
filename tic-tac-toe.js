17004b5b804f190f7e9cbeeffd8bbaedd2fd79a6
Kimski <k_oksana15@hotmail.com>
2019-10-20 22:45:16 -0500

    added X and O on board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,27 @@
 //0 is X and 1 is O
-state = [];
+ const state = ['X', 'O'];
+ var check = 0;
 
 //Load page before js
 window.onload = () => {
   boardtable();
 
+  //Adding an X or O
+  board.addEventListener("click", (e) =>{
+    if (e.target.nodeName === "DIV") {
+
+      if (state[check] == 'X') {
+         e.target.classList.add("X");
+         e.target.innerHTML = "X";
+         check++;
+      } else {
+        e.target.classList.add("O");
+        e.target.innerHTML = "O";
+        check--;
+      }
+    // console.log(e.target.nodeName);
+    }
+  })
 }
 
 //Setting Grids on page + displaying squares
